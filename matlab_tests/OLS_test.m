ceed6a7c6d6d330cddd6931ba035a1280a885bfc
Nate Harada <nharada1@gmail.com>
2014-11-17 13:16:31 -0500

    Approx_ols now returns a vector

--- matlab_tests/OLS_test.m
+++ matlab_tests/OLS_test.m
@@ -1,11 +1,13 @@
-figure()
-
+%% Create data
 % Test data
-X = 0:0.01:100;
-Z = 0:0.01:100;
+X = 0:100;
+Z = linspace(100,0,101);
 Z = Z' * 0.2;
 X = X';
 Y = 0.75 * awgn(X+Z, -3);
+
+%% 1D plots
+figure()
 
 % Plot scatter data
 subplot(1,3,1);
@@ -20,10 +22,10 @@ hold;
 
 % Solve using Matlab's OLS
 subplot(1,3,1);
-solved_matlab = lsqr(X,Y);
-plot(X, X*solved_matlab);
+solved_matlab = lsqr([ones(size(Y)) X],Y);
+plot(X, X*solved_matlab(2) + solved_matlab(1));
 axis([0,100,0,100])
-title('Matlab OLS')
+title(['Matlab OLS' mat2str(solved_matlab)])
 
 % Solve using our non-distributed method.
 A = X'*X;
@@ -59,5 +61,25 @@ title('Distributed OLS')
 figure();
 scatter3(X,Z,Y);
 data_matrix = [X Z Y];
-lsqr([X Z],Y)
+H = [X Z];
+lsqr(H,Y)
 csvwrite('/home/nharada/junkdata/blink_test.csv', data_matrix)
+
+%% Solve using our distributed method
+coeff_dist_matlab = lsqr(H,Y)
+
+m = length(X);
+H = [X Z];
+breaks = linspace(1,m,12);
+A_sum = 0;
+B_sum = 0;
+for i=1:11
+    X_slice = H(breaks(i):breaks(i+1),:);
+    Y_slice = Y(breaks(i):breaks(i+1),:);
+    A_piece = X_slice'*X_slice;
+    B_piece = X_slice'*Y_slice;
+    A_sum = A_sum + A_piece;
+    B_sum = B_sum + B_piece;
+end
+coeff_dist = inv(A_sum)*B_sum
+
